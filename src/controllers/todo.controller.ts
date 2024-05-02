9242017703686686b741ff5e283fcb9f36e2771b
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-05-02 14:37:20 -0300

    chore: add validation quota and active plan user to task with limit plan 5 and with unlimited plan

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -13,11 +13,35 @@ export const createTodoController = async (req: Request, res: Response) => {
   const user = await prisma.user.findUnique({
     where: {
       id: userId as string
+    },
+    select: {
+      id: true,
+      stripeSubscriptionId: true,
+      stripeSubscriptionStatus: true,
+      // Contamos todos os todos que o usuario tem
+      _count: {
+        select: {
+          todos: true
+        }
+      }
     }
   })
 
   if (!user) {
     return res.status(401).send({ message: 'Not authorized' })
+  }
+
+  // Quantidade disponivel para fazer tarefas de graça
+  const hasQuotaAvailable = user._count.todos < 5
+  // Se o usuário tem uma assinatura ativa, ou seja, um id e o status contando como ativo ele tem um plano ativo
+  const hasActivesSubscription =
+    user.stripeSubscriptionId && user.stripeSubscriptionStatus === 'active'
+
+  // Se não tem cota e nem assinatura ativa, não pode criar uma nova tarefa
+  if (!hasQuotaAvailable && !hasActivesSubscription) {
+    return res
+      .status(403)
+      .send({ message: 'Not quota available. Please upgrade your plan' })
   }
 
   const { title, description } = req.body
